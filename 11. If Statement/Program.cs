ba666f7e97fb4c7f1b9e61bc776060bcb768b99f
user <user@LAPTOP-KAN0NU4I>
2023-09-14 21:15:08 +0200

    Add project files.

---
+++ 11. If Statement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.If_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+
+            //NB ! means not, or negation
+            if (isMale)
+            {
+                Console.WriteLine("You are male");
+            }
+            else
+            {
+                Console.WriteLine("You are female");
+            }
+
+            bool isTall = true;
+
+            if(isTall && isMale)
+            {
+                Console.WriteLine("You are tall and a male");
+            }
+
+            else if(!isTall && isMale)
+            {
+                Console.WriteLine("Your are a short male");
+            }
+
+            else if(isTall && !isMale)
+            {
+                Console.WriteLine("You are tall and not male");
+            }
+
+            else if (!(isMale && isTall)) 
+            {
+                Console.WriteLine("You are neither a male nor tall");
+            }
+        }
+    }
+}
